6b7034009cffe2ccb9fdd8e293b919882fce16a7
Akshay Ashok <aks28id@gmail.com>
2020-08-19 13:41:58 +0530

    name fix

--- producer.py
+++ producer.py
@@ -1,11 +1,13 @@
 from kafka import KafkaProducer
 import json
 
+TOPIC_NAME = "maintopic"
+
 producer = KafkaProducer(
     bootstrap_servers=["localhost:9092"],
     value_serializer=lambda x: json.dumps(x).encode("utf-8"),
 )
 
-future = producer.send("my_topic", value="Some message from kafka id")
+future = producer.send(TOPIC_NAME, value="Some message from kafka id")
 result = future.get(timeout=60)
 print(result)
